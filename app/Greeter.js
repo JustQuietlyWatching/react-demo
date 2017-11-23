162bf5145360fb6da74d9042c2e4db173ef67eb0
an_wch <an_wch@suixingpay>
2017-11-23 18:38:34 +0800

    rm

--- app/Greeter.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by an_wch on 2017/9/25.
- */
-const config = require('../config.json');
-module.exports = function () {
-    var greeter = document.createElement('div');
-    greeter.textContent = config.greetText;
-    return greeter;
-}
--- app/main.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by an_wch on 2017/9/25.
- */
-const greeter = require('./Greeter');
-document.querySelector("#content").appendChild(greeter());
